72576027317227d7d4d34730baf0018110456e3a
Aaditya Niranjan <adityaniranjan7@gmail.com>
2026-06-09 12:50:09 +0530

    Initial commit

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+       // System.out.println("Hello, Java!");
+        System.out.println("Hello" +"hii");
+        System.out.print("Hello" + " " + "hii");
+        System.out.println("Hello" +  "hii");
+    }
+}
