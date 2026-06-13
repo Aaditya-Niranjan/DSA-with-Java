5eda14f086627b27ffeba03d7edc3547c4e9bb31
Aaditya Niranjan <adityaniranjan7@gmail.com>
2026-06-13 11:46:08 +0530

    Updated Operators.java

--- Basics/Operators.java
+++ Basics/Operators.java
@@ -29,5 +29,30 @@ public class Operators {
         int c = 20;
         c += 5; // c = c + 5
         System.out.println("c after += 5: " + c);
+
+        float d = 10.5f;
+        float e = 2.5f;
+        System.out.println(d+e);
+
+        //short hand operators
+        int f = 15;     
+        f -= 3; // f = f - 3
+        System.out.println("f after -= 3: " + f);
+        int g = 4;
+        g *= 2; // g = g * 2    
+        System.out.println("g after *= 2: " + g);
+        int h = 20;
+        h /= 4; // h = h / 4
+        System.out.println("h after /= 4: " + h);
+
+        //Unary Operators
+        int activeuser = 5;
+
+        int prefix = ++activeuser; // Increment before use, pehele increment hoga fir use hoga
+        System.out.println("Prefix: " + prefix); // Output: 6
+        int postfix = activeuser++; // Use before increment, pehele use hoga fir increment hoga
+        System.out.println("Postfix: " + postfix); // Output: 6
+        System.out.println("Active User after postfix: " + activeuser); // Output: 7
+
     }
 }
